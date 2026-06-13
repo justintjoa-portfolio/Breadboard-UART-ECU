2ccb716e29427a8b452f272d3328690693f9ed43
Justin Tjoa <justintjoa@Mac.lan>
2026-06-12 20:02:18 -0700

    s

--- embedded-ecu/uart_simulator.cpp
+++ embedded-ecu/uart_simulator.cpp
@@ -4,26 +4,29 @@
 namespace tiny_ecu {
 
     bool UartSimulator::ReceiveByte(std::uint8_t byte) {
-
+        return rx_buffer_.Push(byte);
     }
 
     bool UartSimulator::ReceiveString(const std::string& input) {
+        bool successful = false; 
+        while (rx_buffer_.Pop())
 
     }
 
     bool UartSimulator::ReadByte(std::uint8_t* out) {
-
+        return rx_buffer_.Pop(out);
     }
 
     bool UartSimulator::ReadLine(std::string* out) {
-
+        rx_buffer_.Size();
     }
 
     bool UartSimulator::HasData() const {
-
+        return ! rx_buffer_.Empty();
     }
-    std::size_t UartSimulator::PendingBytes() const {
 
+    std::size_t UartSimulator::PendingBytes() const {
+        return rx_buffer_.Size();
     }
 
 
